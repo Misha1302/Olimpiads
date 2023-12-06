38d0f63951675dc93427d6dbed86c19edb1f57a3
Михаил <misha13022008@gmail.com>
2023-12-06 15:54:15 +0300

    first two tasks

---
+++ Морской бой/Морской бой/Морской бой.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+#define i64 long long
+#define i32 long
+
+int main()
+{
+	i64 m;
+	i64 n;
+
+	std::cin >> n >> m;
+
+	i64* rights = new i64[m];
+	i64* lefts = new i64[n];
+
+	for (i64 i = 0; i < n; i++)
+		std::cin >> lefts[i];
+	
+	for (i64 i = 0; i < m; i++)
+		std::cin >> rights[i];
+
+
+	i64 right = 0;
+	i64 left = 0;
+
+	while (true) {
+		if (right < m && left < n) {
+			if (rights[right] == lefts[left]) {
+				std::cout << 0 << " " << rights[right] << "\n";
+				right++;
+				left++;
+			}
+			else if (rights[right] < lefts[left]) {
+				std::cout << (lefts[left] - rights[right]) / 2 << " " << (rights[right] + lefts[left]) / 2 << "\n";
+
+				right++;
+				left++;
+			}
+			else if (rights[right] >= lefts[left]) {
+				std::cout << 0 << " " << lefts[left] << "\n";
+
+				left++;
+			}
+		}
+		else if (right < m) {
+			for (; right < m; right++)
+				std::cout << 0 << " " << rights[right] << "\n";
+		}
+		else if (left < n) {
+			for (; left < n; left++)
+				std::cout << 0 << " " << lefts[left] << "\n";
+		}
+		else {
+			break;
+		}
+	}
+}
---
+++ Престижный номер/Престижный номер/Престижный номер.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+#define i64 long long 
+#define i32 long
+#define i8 char
+#define str std::string
+#define print(x) std::cout << x;
+
+int main()
+{
+	while (true) {
+		i64 n;
+		std::cin >> n;
+
+		std::vector<i64> digits;
+
+		for (i64 i = 9; i >= 1; i--) {
+			if (n <= 1) {
+				for (i64 i = digits.size() - 1; i >= 0; i--)
+					print(digits[i]);
+
+				print('\n');
+				break;
+			}
+
+			if (i == 1) {
+				print(-1);
+				print('\n');
+				break;
+			}
+
+			if (n % i == 0) {
+				digits.push_back(i);
+				n /= i;
+
+				i = 10;
+				continue;
+			}
+		}
+	}
+}
