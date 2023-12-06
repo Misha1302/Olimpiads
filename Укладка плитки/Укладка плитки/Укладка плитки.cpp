79de29888eeaa75572a18a67ded0d9e7e5845555
Михаил <misha13022008@gmail.com>
2023-12-06 23:36:11 +0300

    укладка плитки

---
+++ Укладка плитки/Укладка плитки/Укладка плитки.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+#define f64 double
+#define i64 long long
+#define i32 long
+#define bool char
+
+int main()
+{
+	i64 length, width;
+	i64 aDiagonal, bDiagonal;
+	i64 packaging;
+
+	std::cin >> length >> width;
+	std::cin >> aDiagonal >> bDiagonal;
+	std::cin >> packaging;
+
+	i64 square = (width / bDiagonal * (length / aDiagonal))
+		*
+		((width / bDiagonal - 1) * (length / aDiagonal) - 1);
+
+	i64 delta = square % packaging;
+	square += delta;
+
+	std::cout << square / packaging;
+}
