327edab21f07e7c30b33f46baef9ff2cebf95a82
Михаил <misha13022008@gmail.com>
2023-12-11 22:26:45 +0300

    Шахматы

---
+++ Шахматы/Шахматы/Шахматы.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+#define i64 long long
+
+int main()
+{
+	i64 n;
+	std::cin >> n;
+
+	i64 count = 0;
+
+	for (size_t i = 0; i < n; i++) {
+		i64 len;
+		std::cin >> len;
+
+		if (len <= i)
+			break;
+
+		count++;
+	}
+
+	std::cout << count << "\n";
+	for (size_t i = 0; i < count; i++)
+		std::cout << i + 1 << " " << i + 1 << "\n";
+}
