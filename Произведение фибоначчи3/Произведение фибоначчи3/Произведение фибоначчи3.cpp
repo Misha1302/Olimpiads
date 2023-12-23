66ee0d55354717caa83e7fa4d91920ab366be9b5
Михаил <misha13022008@gmail.com>
2023-12-23 12:45:29 +0300

    Произведение фибоначчи и разделение прямоугольника

---
+++ Произведение фибоначчи3/Произведение фибоначчи3/Произведение фибоначчи3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+#define i64 long long
+#define u64 unsigned long long
+#define size_t i64
+
+std::vector<i64> fibs;
+
+// генерация чисел фибоначчи
+void genFibs() {
+    fibs = std::vector<i64>();
+    fibs.push_back(2);
+    fibs.push_back(3);
+
+    i64 i = 2;
+    while ((u64)fibs[i - 1] + (u64)fibs[i - 2] < ((1ull << 63ull) - 1ull)) {
+        fibs.push_back(fibs[i - 1] + fibs[i - 2]);
+        i++;
+    }
+}
+
+
+// обход графа в глубину
+i64 sum(i64 n, i64 index) {
+    if (n == 1) // если достигли конечной точки
+        return 1; // конечная точка - 1
+
+    i64 s = 0; // переменная для хранения суммы
+    // перебираем числа от index до момента, пока не найдем число в ряду фибоначчи больше n, или пока числа не закончатся 
+    for (size_t i = index; i < fibs.size() && n >= fibs[i]; i++)
+    {
+        // если нашли число, которое способно делиться на число фибоначчи без остатка
+        if (n % fibs[i] == 0) {
+            // то запускаем ветвь с более маленьким числом (64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1)
+            s += sum(n / fibs[i], i);
+        }
+    }
+
+    // возвращаем итоговую сумму
+    return s;
+}
+
+int main()
+{
+    genFibs();
+
+    i64 n = 10e17;
+    std::cout << sum(n, 0);
+}
---
+++ Разделение прямоугольника/Разделение прямоугольника/Разделение прямоугольника.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+#define i64 long long
+#define size_t i64
+
+void FindNumbers(i64 k, i64 m, i64 width, i64 height, i64& horizontals, i64& verticals) {
+	/*if ((width + 1) * (k - width + 1) < m)
+		goto end;
+
+	for (size_t first = std::max(k - height + 1, 0ll); first < width; first++)
+	{
+		i64 second = k - first;
+
+		if ((first + 1) * (second + 1) == m) {
+			horizontals = first;
+			verticals = second;
+
+			return;
+		}
+	}
+
+
+end:
+	horizontals = -1;
+	verticals = -1;*/
+
+	i64 D = k * k - 4 * (-1) * (k - m + 1);
+
+	if (D <= 0) goto error;
+
+	horizontals = (-k + sqrt(D)) / -2;
+	verticals = (-k - sqrt(D)) / -2;
+
+	if (horizontals >= width || verticals >= height) goto error;
+
+	return;
+
+error:
+	horizontals = -1;
+	verticals = -1;
+}
+
+int main()
+{
+	i64 t, a, b, k, m;
+
+	std::cin >> t;
+
+	for (size_t i = 0; i < t; i++)
+	{
+		std::cin >> a >> b >> k >> m;
+
+		i64 n1, n2;
+		auto t = clock();
+		FindNumbers(k, m, a, b, n1, n2);
+		std::cout << clock() - t << " --- ";
+
+		if (n1 == -1 || n2 == -1)
+			std::cout << "-1" << "\n";
+		else std::cout << n1 << " " << n2 << "\n";// << " " << n1 + n2 << " " << (n1 + 1) * (n2 + 1) << "\n";
+	}
+}
