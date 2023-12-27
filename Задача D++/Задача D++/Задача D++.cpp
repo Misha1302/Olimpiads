da325ba81048b46175caebe4d1b216d7ca1e661e
Михаил <misha13022008@gmail.com>
2023-12-27 16:12:45 +0300

    Полное решение задачи D++!

--- Задача D++/Задача D++/Задача D++.cpp
+++ Задача D++/Задача D++/Задача D++.cpp
@@ -2,136 +2,95 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <map>
 
 
 #define i16 short
 #define i8 char
+#define zero i8
+#define size_t i16
 
-
-struct var {
-	i16 value;
-};
-
-struct el {
-	i16 value;
-};
-
-struct swapCmd {
-	i16 ind1;
-	i16 ind2;
-};
-
-
-std::ostream& operator <<(std::ostream& a, var b) { return a << b.value; }
-std::ostream& operator <<(std::ostream& a, el b) { return a << b.value; }
-
-
-el items[10'000];
-var vars[26];
 i16 N;
-std::vector<swapCmd> swaps;
+i16 items[10'000];
 
-void printInfo() {
-	std::cout << "array: ";
-	for (size_t i = 0; i < N; i++)
-		std::cout << items[i] << ", ";
-	std::cout << "\b\b --- ";
-
-	std::cout << "vars: ";
-	for (size_t i = 0; i < 3; i++)
-		std::cout << vars[i] << ", ";
-	std::cout << "\b\b \n";
-}
-
-void swapEls(i16 el1, i16 el2) {
-	//std::cout << "a" << "<-" << el1 << "\n";
-	//std::cout << el1 << "<-" << el2 << "\n";
-	//std::cout << el2 << "<-" << "a" << "\n";
-
-	auto temp = items[el1];
-	items[el1] = items[el2];
-	items[el2] = temp;
-
-	//printInfo();
-}
-
-void swapsGenerator() {
-	i16 greaterThan = -1;
-
-	for (i16 i = 0; i < N; i++)
+void PrintBlocks(std::vector<std::vector<i16>> blocks) {
+	return;
+	for (size_t i = 0; i < blocks.size(); i++)
 	{
-		if (i == items[i].value)
-			continue;
+		std::cout << i + 1 << ". ";
 
-		i16 minInd = i;
-		for (i16 j = 0; j < N; j++)
-		{
-			if (greaterThan < items[j].value && items[j].value < items[minInd].value)
-				minInd = items[j].value;
+		for (size_t j = 0; j < blocks[i].size(); j++) {
+			std::cout << blocks[i][j] << " ";
 		}
 
-
-		if (i != minInd) {
-			// swapCmd cmd = { std::min(i + 1, minInd + 1), std::max(i + 1, minInd + 1) };
-			swapCmd cmd = { i + 1, minInd + 1 };
-			swaps.push_back(cmd);
-			swapEls(i, minInd);
-		}
-
-		greaterThan = items[i].value;
+		std::cout << "\n";
 	}
-
 }
 
-void printOptimized() {
+std::vector<std::vector<i16>> SplitIntoBlock() {
+	std::vector<std::vector<i16>> blocks;
+
+	std::vector<i16> mapp;
+
+	size_t i = 0;
+	i16 ind = i;
+	while (i < N)
+	{
+		i16 j = items[i];
+
+		for (auto& el : mapp)
+			if (el == j)
+				goto next;
+
+		blocks.push_back(std::vector<i16>());
+
+		j = i + 1;
+		do {
+			mapp.push_back(j);
+			blocks[ind].push_back(j);
+			j = items[j - 1];
+		} while (j != i + 1);
+		blocks[ind].push_back(j);
+
+		ind++;
+	next:
+		i++;
+	}
+
+	return blocks;
+}
+
+i16 CountLen(std::vector<std::vector<i16>> blocks) {
 	i16 len = 0;
-	for (i16 i = 0; i < swaps.size(); i++) {
-		auto& e = swaps[i];
 
-		len++;
-		// std::cout << "a" << "<-" << e.ind1 << "\n";
-		len++;
-		// std::cout << e.ind1 << "<-" << e.ind2 << "\n";
+	for (size_t i = 0; i < blocks.size(); i++) {
+		bool a = blocks[i].size() < 2;
+		if (a) continue;
+		bool b = blocks[i][0] == blocks[i][1];
+		if (b) continue;
 
-		while
-			(
-				i + 1 < swaps.size() &&
-				(
-					swaps[i].ind2 == swaps[(i16)i + (i16)1].ind1
-					|| swaps[i].ind2 == swaps[(i16)i + (i16)1].ind1
-					)
-				)
-		{
-			i++;
-			e = swaps[i];
-			// std::cout << e.ind1 << "<-" << e.ind2 << "\n";
-			len++;
-			//std::cout << "opt";
-		}
-
-		// std::cout << e.ind2 << "<-" << "a" << "\n";
-		len++;
-		// std::cout << "\n";
+		len += blocks[i].size();
 	}
-	std::cout << len;
+
+	return len;
 }
 
-void printNotOptimized() {
-	for (i16 i = 0; i < swaps.size(); i++) {
-		auto& e = swaps[i];
-		std::cout << "swap " << e.ind1 << " " << e.ind2 << "\n";
-	}
+void Execute() {
+	auto blocks = SplitIntoBlock();
+	PrintBlocks(blocks);
+
+	std::cout << CountLen(blocks);
+}
+
+void Input() {
+	std::cin >> N;
+
+	for (size_t i = 0; i < N; i++)
+		std::cin >> items[i];
 }
 
 int main()
 {
-	swaps = std::vector<swapCmd>();
-	/*std::cin >> N;
-
-	for (size_t i = 0; i < N; i++)
-		std::cin >> items[i].value;*/
-
-
 	for (size_t i = 1; i <= 18; i++)
 	{
 		auto s = std::to_string(i);
@@ -142,16 +101,12 @@ int main()
 		std::ifstream ans("F:\\DOWNLOADS\\7-d\\D\\Answer" + s + ".txt");
 
 		in >> N;
-		swaps = std::vector<swapCmd>();
 
 		for (size_t i = 0; i < N; i++)
-			in >> items[i].value;
+			in >> items[i];
+		// Input();
 
-
-		std::cout << i << ". ";
-		swapsGenerator();
-
-		printOptimized();
+		Execute();
 
 		i16 temp;
 		ans >> temp;
